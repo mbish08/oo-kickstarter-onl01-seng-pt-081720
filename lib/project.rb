25a07d379e41026288612f3378b717d8e0266a60
Mikel Bishop <mdrew8@gmail.com>
2020-09-19 22:29:03 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -12,6 +12,7 @@ class Project
   def add_backer(name)
     @backers << name
    # binding.pry 
+   if backer.back
     backer.back_project(self) unless backer.backed_projects.include?(self)
   end 
   
